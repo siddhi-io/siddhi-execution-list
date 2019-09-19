cb825ef343d5f2c076b4ee92228c2e7a435d9ffe
Niveathika <niveathika@wso2.com>
2019-09-20 00:44:26 +0530

    Add list:contains() implementation

---
+++ component/src/main/java/io/siddhi/extension/execution/list/ContainsFunctionExtension.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c)  2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.execution.list;
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ParameterOverload;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.executor.function.FunctionExecutor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.Attribute;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * contains(Object)
+ */
+@Extension(
+        name = "contains",
+        namespace = "list",
+        description = "Function checks if the list contains the value.",
+        parameters = {
+                @Parameter(name = "list",
+                        description = "The list that needs to be checked on whether it contains the value or not.",
+                        type = {DataType.OBJECT},
+                        dynamic = true
+                ),
+                @Parameter(name = "value",
+                        description = "The value to be checked.",
+                        type = {DataType.INT, DataType.LONG, DataType.FLOAT, DataType.DOUBLE,
+                                DataType.BOOL, DataType.STRING},
+                        dynamic = true
+                )
+        },
+        parameterOverloads = {
+                @ParameterOverload(parameterNames = {"list", "value"})
+        },
+        returnAttributes = @ReturnAttribute(
+                description = "Returns `true` if the list contains the value and `false` if otherwise.",
+                type = DataType.BOOL),
+        examples = @Example(
+                syntax = "list:contains(students, 'Sam')",
+                description = "Returns 'true' if the students list contains value `Sam` else it returns `false`.")
+)
+public class ContainsFunctionExtension extends FunctionExecutor<State> {
+    private Attribute.Type returnType = Attribute.Type.BOOL;
+
+    @Override
+    protected StateFactory<State> init(ExpressionExecutor[] attributeExpressionExecutors,
+                                ConfigReader configReader,
+                                SiddhiQueryContext siddhiQueryContext) {
+        return null;
+    }
+
+    @Override
+    protected Object execute(Object[] data, State state) {
+        if (data[0] instanceof List) {
+            return ((List) data[0]).contains(data[1]);
+        }
+        throw new SiddhiAppRuntimeException("First attribute value must be of type java.util.List.");
+    }
+
+    @Override
+    protected Object execute(Object data, State state) {
+        return null;
+    }
+
+    @Override
+    public Attribute.Type getReturnType() {
+        return returnType;
+    }
+}
