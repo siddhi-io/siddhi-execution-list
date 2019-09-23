53d0f05ce115ccf48a3eb5c6a898e6396e005ac1
Niveathika <niveathika@wso2.com>
2019-09-23 19:47:57 +0530

    Add implementation of list:tokenize()

---
+++ component/src/main/java/io/siddhi/extension/execution/list/TokenizeStreamProcessor.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.execution.list;
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.annotation.Parameter;
+import io.siddhi.annotation.ParameterOverload;
+import io.siddhi.annotation.ReturnAttribute;
+import io.siddhi.annotation.util.DataType;
+import io.siddhi.core.config.SiddhiQueryContext;
+import io.siddhi.core.event.ComplexEventChunk;
+import io.siddhi.core.event.stream.MetaStreamEvent;
+import io.siddhi.core.event.stream.StreamEvent;
+import io.siddhi.core.event.stream.StreamEventCloner;
+import io.siddhi.core.event.stream.holder.StreamEventClonerHolder;
+import io.siddhi.core.event.stream.populater.ComplexEventPopulater;
+import io.siddhi.core.exception.SiddhiAppRuntimeException;
+import io.siddhi.core.executor.ExpressionExecutor;
+import io.siddhi.core.query.processor.ProcessingMode;
+import io.siddhi.core.query.processor.Processor;
+import io.siddhi.core.query.processor.stream.StreamProcessor;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.query.api.definition.AbstractDefinition;
+import io.siddhi.query.api.definition.Attribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation class for map:tokenize()
+ */
+@Extension(
+        namespace = "list",
+        name = "tokenize",
+        description = "Tokenize the list and return each key, value as new attributes in events",
+        parameters = {
+                @Parameter(
+                        name = "list",
+                        description = "Array list which needs to be tokenized",
+                        type = DataType.OBJECT,
+                        dynamic = true
+                )
+        },
+        parameterOverloads = {
+                @ParameterOverload(parameterNames = {"list"}),
+                @ParameterOverload(parameterNames = {"list", "..."})
+        },
+        returnAttributes = {
+                @ReturnAttribute(
+                        name = "index",
+                        description = "Index of an entry consisted in the list",
+                        type = {DataType.INT}
+                ),
+                @ReturnAttribute(
+                        name = "value",
+                        description = "Value of an entry consisted in the list",
+                        type = {DataType.OBJECT}
+                )
+        },
+        examples = {
+                @Example(
+                        syntax = "list:tokenize(customList)",
+                        description = "If custom list contains ('WSO2', 'IBM', 'XYZ') elements, " +
+                                "then tokenize function will return 3 events with value attributes WSO2, IBM and XYZ " +
+                                "respectively."
+                )
+        }
+)
+public class TokenizeStreamProcessor extends StreamProcessor<State> {
+   private List<Attribute> attributesList = new ArrayList<>();
+   private ExpressionExecutor[] expressionExecutor;
+   private int numList;
+
+    @Override
+    protected StateFactory<State> init(MetaStreamEvent metaStreamEvent, AbstractDefinition abstractDefinition,
+                                       ExpressionExecutor[] expressionExecutors, ConfigReader configReader,
+                                       StreamEventClonerHolder streamEventClonerHolder,
+                                       boolean outputExpectsExpiredEvents, boolean findToBeExecuted,
+                                       SiddhiQueryContext siddhiQueryContext) {
+        this.expressionExecutor = expressionExecutors;
+        this.numList = expressionExecutors.length;
+
+        this.attributesList.add(new Attribute("index", Attribute.Type.INT));
+        this.attributesList.add(new Attribute("value", Attribute.Type.OBJECT));
+        return null;
+    }
+
+    @Override
+    protected void process(ComplexEventChunk<StreamEvent> complexEventChunk, Processor processor,
+                           StreamEventCloner streamEventCloner, ComplexEventPopulater complexEventPopulater,
+                           State state) {
+        while (complexEventChunk.hasNext()) {
+            StreamEvent event = complexEventChunk.next();
+
+            List<List<Object>> dataList = new ArrayList<>();
+            for (int i = 0; i < this.expressionExecutor.length; i++) {
+                Object listObject = this.expressionExecutor[i].execute(event);
+                if (!(listObject instanceof List)) {
+                    throw new SiddhiAppRuntimeException("Attribute number, '" + (i + 1) + "' must be of type " +
+                            "java.util.List, but found " + listObject.getClass().getCanonicalName());
+                }
+                dataList.add((List) listObject);
+            }
+
+            int maxIndex = 0;
+            for (List<Object> dataElement : dataList) {
+                if (dataElement.size() > maxIndex) {
+                    maxIndex = dataElement.size();
+                }
+            }
+
+            for (int i = 0; i < maxIndex; i++) {
+                StreamEvent clonedEvent = streamEventCloner.copyStreamEvent(event);
+                Object[] data;
+                if (this.numList == 1) {
+                    data = new Object[]{i, dataList.get(0).get(i)};
+                } else {
+                    List<Object> value = new ArrayList<>();
+                    for (int j = 0; j < this.numList; j++) {
+                        if (dataList.get(j).size() > i) {
+                            value.add(dataList.get(j).get(i));
+                        } else {
+                            value.add(null);
+                        }
+                    }
+                    data = new Object[]{i, value};
+                }
+
+                complexEventPopulater.populateComplexEvent(clonedEvent, data);
+                complexEventChunk.insertBeforeCurrent(clonedEvent);
+            }
+            complexEventChunk.remove();
+        }
+        nextProcessor.process(complexEventChunk);
+    }
+
+    @Override
+    public List<Attribute> getReturnAttributes() {
+        return this.attributesList;
+    }
+
+    @Override
+    public ProcessingMode getProcessingMode() {
+        return ProcessingMode.SLIDE;
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+}
---
+++ component/src/test/java/io/siddhi/extension/execution/list/TokenizeStreamProcessorTestCase.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c)  2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.siddhi.extension.execution.list;
+
+import io.siddhi.core.SiddhiAppRuntime;
+import io.siddhi.core.SiddhiManager;
+import io.siddhi.core.event.Event;
+import io.siddhi.core.exception.SiddhiAppCreationException;
+import io.siddhi.core.stream.output.StreamCallback;
+import io.siddhi.core.util.EventPrinter;
+import io.siddhi.core.util.SiddhiTestHelper;
+import org.apache.log4j.Logger;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TokenizeStreamProcessorTestCase {
+    private static final Logger log = Logger.getLogger(TokenizeStreamProcessorTestCase.class);
+    private AtomicInteger count = new AtomicInteger(0);
+    private volatile boolean eventArrived;
+    private List<Object[]> inEventsList;
+
+    @BeforeMethod
+    public void init() {
+        count.set(0);
+        eventArrived = false;
+        inEventsList = new ArrayList<>();
+    }
+
+    @Test
+    public void testTokenizeStreamProcessor1() throws InterruptedException {
+        log.info("Tokenize Stream Processor TestCase");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "\ndefine trigger startTrigger at 'start';";
+        String query = (
+                " @info(name = 'query1') "
+                + "from startTrigger "
+                + "select list:create('one', 'two', 'three', 'four' , 'five' , 'six') as list "
+                + "insert into tmpStream;"
+                + "@info(name = 'query2') "
+                + "from tmpStream#list:tokenize(list) "
+                + "select index, value "
+                + "insert into TokenizedStream; "
+        );
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(
+                inStreamDefinition + query);
+        siddhiAppRuntime.addCallback("TokenizedStream", new StreamCallback() {
+            @Override
+            public void receive(Event[] events) {
+                EventPrinter.print(events);
+                for (Event event : events) {
+                    eventArrived = true;
+                    count.incrementAndGet();
+                    inEventsList.add(event.getData());
+                }
+            }
+        });
+        siddhiAppRuntime.start();
+        SiddhiTestHelper.waitForEvents(100, 6, count, 60000);
+
+        List<Object[]> expected = Arrays.asList(
+                new Object[]{0, "one"},
+                new Object[]{1, "two"},
+                new Object[]{2, "three"},
+                new Object[]{3, "four"},
+                new Object[]{4, "five"},
+                new Object[]{5, "six"}
+                );
+        AssertJUnit.assertTrue(eventArrived);
+        AssertJUnit.assertEquals(6, count.get());
+        AssertJUnit.assertTrue("Events do not match", SiddhiTestHelper.isEventsMatch(expected, inEventsList));
+
+        siddhiAppRuntime.shutdown();
+    }
+
+    @Test(expectedExceptions = SiddhiAppCreationException.class)
+    public void testTokenizeStreamProcessor2() {
+        log.info("Tokenize Stream Processor TestCase - More than one attribute");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "\ndefine trigger startTrigger at 'start';";
+        String query = (
+                "@info(name = 'query1') "
+                        + "from startTrigger "
+                        + "select list:create(1, 2,3) as list1 "
+                        + "insert into tmpStream;"
+                        + "@info(name = 'query2') "
+                        + "from tmpStream#list:tokenize(list1, '2') "
+                        + "select key, value "
+                        + "insert into TokenizedStream; "
+
+        );
+
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(
+                inStreamDefinition + query);
+        siddhiAppRuntime.shutdown();
+    }
+
+    @Test
+    public void testTokenizeStreamProcessor3() throws InterruptedException {
+        log.info("Tokenize Stream Processor TestCase - Use string object");
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        String inStreamDefinition = "\ndefine trigger startTrigger at 'start';";
+        String query = (
+                "@info(name = 'query1') "
+                + "from startTrigger "
+                + "select list:create('one', 'two', 'three', 'seven') as list1, " +
+                        "list:create('four','five','six') as list2 "
+                + "insert into tmpStream;"
+                + "@info(name = 'query2') "
+                + "from tmpStream#list:tokenize(list1, list2) "
+                + "select index, value "
+                + "insert into TokenizedStream; "
+
+        );
+        SiddhiAppRuntime siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(
+                inStreamDefinition + query);
+        siddhiAppRuntime.addCallback("TokenizedStream", new StreamCallback() {
+            @Override
+            public void receive(Event[] events) {
+                EventPrinter.print(events);
+                for (Event event : events) {
+                    eventArrived = true;
+                    count.incrementAndGet();
+                    inEventsList.add(event.getData());
+                }
+            }
+        });
+        siddhiAppRuntime.start();
+        SiddhiTestHelper.waitForEvents(100, 3, count, 60000);
+
+        List<Object[]> expected = Arrays.asList(
+                new Object[]{0, new ArrayList<>(Arrays.asList("one", "four"))},
+                new Object[]{1, new ArrayList<>(Arrays.asList("two", "five"))},
+                new Object[]{2, new ArrayList<>(Arrays.asList("three", "six"))},
+                new Object[]{3, new ArrayList<>(Arrays.asList("seven", null))}
+        );
+        AssertJUnit.assertTrue(eventArrived);
+        AssertJUnit.assertEquals(4, count.get());
+        AssertJUnit.assertTrue("Events do not match", SiddhiTestHelper.isUnsortedEventsMatch(expected, inEventsList));
+
+        siddhiAppRuntime.shutdown();
+    }
+}
